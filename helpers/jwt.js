3188d5831c0d0e5b3e02be6fac4fff9c4c9064be
kymani20100 <kymani20100@gmail.com>
2022-01-20 14:42:11 +0000

    Modifications to the endpoints

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -11,7 +11,8 @@ function authJwt() {
     }).unless({
         path: [
             { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
-            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS']},
+            { url: /\/api\/v1\/products/, methods: ['POST', 'OPTIONS']},
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
             { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
